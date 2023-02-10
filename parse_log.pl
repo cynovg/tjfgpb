b367570cf3a6d73abab2a78ebe65fe4c1ef33678
Cyrill Novgorodcev <cynovg@pause.org>
2023-02-10 16:35:16 +0300

    parse log

--- parse_log.pl
+++ parse_log.pl
@@ -12,13 +12,52 @@ use TJFGPB::Utils qw(get_dbh);
 const my $logname => "out";
 
 sub main {
-	my $dbh = get_dbh();
+	store_log(parse_log($logname));
+}
 
-	open(my $fh, "<", $logname) or die "Cant open log: $!\n";
+sub store_log {
+	my ($messages, $log) = @_;
+	return;
+}
+
+sub parse_log {
+	my ($filename) = @_;
+	my (@log, @messages);
+	open(my $fh, "<", $filename) or die "Cant open log: $!\n";
 	while (<$fh>) {
 		chomp;
-		say
+		s/^(?<timestamp>\S+\s+\S+)\s+(?<int_id>\S+)\s+//;
+		my $timestamp = $+{'timestamp'};
+		my $int_id    = $+{'int_id'};
+		if (/^(?<flag>(?:<=|=>|->|\*\*|==))\s+(?<address>\S+)\s+(?<message>.*)$/) {
+			my $flag    = $+{'flag'};
+			my $address = $+{'address'};
+			my $message = $+{'message'};
+			if ($flag eq "<=") {
+				push @messages, {
+					timestamp => $timestamp,
+					int_id    => $int_id,
+					str       => sprintf("%s %s %s %s", $int_id, $flag, $address, $message),
+				};
+			}
+			else {
+				push @log, {
+					timestamp => $timestamp,
+					int_id    => $int_id,
+					address   => $address,
+					str       => $message,
+				};
+			}
+		}
+		else {
+			push @log, {
+				timestamp => $timestamp,
+				int_id    => $int_id,
+				str       => $_,
+			};
+		}
 	}
+	return \@messages, \@log;
 }
 
 main(@ARGV);
