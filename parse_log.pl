d7d55699effdc5a1d738c87204ac3af2e87eb1cf
Cyrill Novgorodcev <cynovg@pause.org>
2023-02-10 15:06:16 +0300

    read log

--- parse_log.pl
+++ parse_log.pl
@@ -2,15 +2,23 @@
 use utf8;
 use v5.30;
 use warnings;
+use Const::Fast;
 
 use FindBin qw( $Bin );
 use lib "$Bin/lib/";
 
 use TJFGPB::Utils qw(get_dbh);
 
+const my $logname => "out";
+
 sub main {
 	my $dbh = get_dbh();
-	use DDP; die np $dbh;
+
+	open(my $fh, "<", $logname) or die "Cant open log: $!\n";
+	while (<$fh>) {
+		chomp;
+		say
+	}
 }
 
 main(@ARGV);
