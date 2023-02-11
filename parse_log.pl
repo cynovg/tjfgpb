bb37adac3f1e8bbc23a479f70b2d71819836babf
Cyrill Novgorodcev <cynovg@pause.org>
2023-02-11 12:43:22 +0300

    add other to log

--- parse_log.pl
+++ parse_log.pl
@@ -13,17 +13,38 @@ use TJFGPB::Process qw(fill_parts);
 const my $logname => "out";
 
 sub main {
-	my ($messages_count, $logs_count) = store_log(parse_log($logname));
+	my ($messages_count, $logs_count) = store_recs(parse_log($logname));
 	printf("MESSAGES ADDED [%s]\n", $messages_count);
+	printf("LOGS ADDED [%s]\n", $logs_count);
 }
 
-sub store_log {
+sub store_recs {
 	my ($parts) = @_;
 
 	my $dbh = get_dbh();
 	my $messages_count = store_messages($dbh, $parts->{'message'});
+	my $logs_count = store_logs($dbh, $parts->{'log'});
 
-	return $messages_count;
+	return $messages_count, $logs_count;
+}
+
+sub store_logs {
+	my ($dbh, $logs) = @_;
+	my ($logs_count);
+	while (my @part = splice(@$logs, 0, 500)) {
+		my $values = join(", ", map {
+			sprintf("( %s )",
+				join(",",
+					$dbh->quote($_->{'timestamp'}),
+					$dbh->quote($_->{'id'}),
+					$dbh->quote($_->{'str'}),
+					$dbh->quote($_->{'address'}),
+				)
+			)
+		} @part);
+		$logs_count += $dbh->do("INSERT INTO `log` (`created`, `int_id`, `str`, `address`) VALUES " . $values);
+	}
+	return $logs_count;
 }
 
 sub store_messages {
