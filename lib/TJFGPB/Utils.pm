f4d87b6597ac9d1008f4da18054c8b6d6850bc25
Cyrill Novgorodcev <cynovg@pause.org>
2023-02-11 12:53:21 +0300

    move some sub to utils

--- lib/TJFGPB/Utils.pm
+++ lib/TJFGPB/Utils.pm
@@ -7,7 +7,7 @@ use Const::Fast;
 use DBI;
 
 our @ISA       = qw(Exporter);
-our @EXPORT_OK = qw(get_dbh);
+our @EXPORT_OK = qw(get_dbh store_logs store_messages);
 
 const my $database => 'content';
 const my $host     => '127.0.0.1';
@@ -21,6 +21,45 @@ sub get_dbh {
     $dbh->{'mysql_enable_utf8mb4'} = 1;
 
     return $dbh;
+}
+
+sub store_logs {
+	my ($dbh, $logs) = @_;
+	my ($logs_count);
+	while (my @part = splice(@$logs, 0, 500)) {
+		my $values = join(", ", map {
+			sprintf("( %s )",
+				join(",",
+					$dbh->quote($_->{'timestamp'}),
+					$dbh->quote($_->{'id'}),
+					$dbh->quote($_->{'str'}),
+					$dbh->quote($_->{'address'}),
+				)
+			)
+		} @part);
+		$logs_count += $dbh->do("INSERT INTO `log` (`created`, `int_id`, `str`, `address`) VALUES " . $values);
+	}
+	return $logs_count;
+}
+
+sub store_messages {
+	my ($dbh, $messages) = @_;
+
+	my ($messages_count);
+	while (my @part = splice(@$messages, 0, 500)) {
+		my $values = join(", ", map {
+			sprintf("( %s )",
+				join(",",
+					$dbh->quote($_->{'timestamp'}),
+					$dbh->quote($_->{'id'}),
+					$dbh->quote($_->{'int_id'}),
+					$dbh->quote($_->{'str'}),
+				)
+			)
+		} @part);
+		$messages_count += $dbh->do("INSERT INTO `message` (`created`, `id`, `int_id`, `str`) VALUES " . $values);
+	}
+	return $messages_count;
 }
 
 1;
--- parse_log.pl
+++ parse_log.pl
@@ -7,7 +7,7 @@ use Const::Fast;
 use FindBin qw( $Bin );
 use lib "$Bin/lib/";
 
-use TJFGPB::Utils qw(get_dbh);
+use TJFGPB::Utils qw(get_dbh store_logs store_messages);
 use TJFGPB::Process qw(fill_parts);
 
 const my $logname => "out";
@@ -24,45 +24,6 @@ sub store_recs {
 	store_messages($dbh, $parts->{'message'});
 	store_logs($dbh, $parts->{'log'});
 
-}
-
-sub store_logs {
-	my ($dbh, $logs) = @_;
-	my ($logs_count);
-	while (my @part = splice(@$logs, 0, 500)) {
-		my $values = join(", ", map {
-			sprintf("( %s )",
-				join(",",
-					$dbh->quote($_->{'timestamp'}),
-					$dbh->quote($_->{'id'}),
-					$dbh->quote($_->{'str'}),
-					$dbh->quote($_->{'address'}),
-				)
-			)
-		} @part);
-		$logs_count += $dbh->do("INSERT INTO `log` (`created`, `int_id`, `str`, `address`) VALUES " . $values);
-	}
-	return $logs_count;
-}
-
-sub store_messages {
-	my ($dbh, $messages) = @_;
-
-	my ($messages_count);
-	while (my @part = splice(@$messages, 0, 500)) {
-		my $values = join(", ", map {
-			sprintf("( %s )",
-				join(",",
-					$dbh->quote($_->{'timestamp'}),
-					$dbh->quote($_->{'id'}),
-					$dbh->quote($_->{'int_id'}),
-					$dbh->quote($_->{'str'}),
-				)
-			)
-		} @part);
-		$messages_count += $dbh->do("INSERT INTO `message` (`created`, `id`, `int_id`, `str`) VALUES " . $values);
-	}
-	return $messages_count;
 }
 
 sub parse_log {
