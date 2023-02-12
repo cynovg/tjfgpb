52e652956340e805f60af84f1df9146397e4d988
Cyrill Novgorodcev <cynovg@pause.org>
2023-02-12 11:43:07 +0300

    use HTML::Template

--- cgi-bin/search.pl
+++ cgi-bin/search.pl
@@ -3,10 +3,8 @@ use utf8;
 use v5.30;
 use warnings;
 use CGI;
-use File::Slurp;
 use Const::Fast;
-use HTML::Table;
-use HTML::Escape qw/escape_html/;
+use HTML::Template;
 
 use FindBin qw( $Bin );
 use lib "$Bin/../lib/";
@@ -14,34 +12,26 @@ use lib "$Bin/../lib/";
 use TJFGPB::Utils qw(get_dbh);
 use TJFGPB::Search qw(search);
 
-const my $root => "/var/www/html/";
+const my $template_name => "/var/www/html/templates/search_form.tmpl";
 
 sub main {
 	my $cgi = CGI->new;
 	print $cgi->header(-charset => 'utf8');
+	my $template = HTML::Template->new(filename => $template_name);
 	if ($cgi->request_method eq 'POST' && (my $address = scalar $cgi->param("address"))) {
 		my $dbh = get_dbh();
 		$address = 'error' if $address !~ /^[\w\d\@.-:]+\Z/;
 		my ($count, $result) = search($dbh, address => $address);
-		my $html = read_file($root . "templates/search_form.html");
+		$template->param(RESULT => 1);
+		$template->param(COUNT  => $count);
+		$template->param(SEARCH => $address);
 		if ($count) {
-			my $table = HTML::Table->new(
-				-cols => 1,
-				-head => [ qw/message/ ],
-			);
-			for (@$result) {
-				$table->addRow("<pre>" . escape_html($_) . "</pre>");
-			}
-			$table .= "<br />\n<div>\n\t<p>total $count</p>\n</div>\n" if $count > 100;
-			$html =~ s/<!-- RESULT -->/$table/;
-		} else {
-			my $not_found = "<div>\n<p>Address <i><b>$address</b></i> not found</p>\n</div>\n";
-			$html =~ s/<!-- RESULT -->/$not_found/;
+			$template->param(ROWS => [map { ROW => $_ }, @$result]);
+			$template->param(MORE => $count) if $count > 100;
 		}
-		print $html;
-	} else {
-		print read_file($root . "templates/search_form.html");
 	}
+	print $template->output();
+	return;
 }
 
 main(@ARGV);
